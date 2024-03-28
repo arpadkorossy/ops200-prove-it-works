2b5feabab7f6e23689f45aa563e303acd859d3f1
arpadkorossy <korossy.arpad@gmail.com>
2024-03-28 17:09:43 -0400

    Second commit - e2e testing still broken

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expect = require('chai').expect;
 const path = require('path');
 const Nightmare = require('nightmare');
+const assert = require('assert');
 
 const app = express();
 
@@ -10,9 +11,10 @@ app.use(express.static(path.join(__dirname, '../../dist')));
 
 const url = 'http://localhost:8888';
 
-const nightmare = new Nightmare();
+let nightmare = null;
+beforeEach(() => { nightmare = new Nightmare() })
 
-describe('End to End Tests', () => {
+describe('End to End Tests', () => { 
     let httpServer = null;
     let pageObject = null;
 
@@ -30,12 +32,45 @@ describe('End to End Tests', () => {
         done();
     });
 
+    it('should return 200 status', function (done) {
+        new Nightmare()
+            .goto(url)
+            .then(function (response) {
+                response.code.should.equal(200);
+                done();
+            });
+    });
+
     it('should contain a <h1> element for the page title', () => { 
         return pageObject
             .evaluate(() => document.querySelector('h1').innerText)
             .then(headerText => {
-            expect(headerText).to.not.be.null;
-            expect(headerText).to.equal('Mortgage Calculator');
+                expect(headerText).to.not.be.null;
+                headerText.should.equal('Mortgage Calculator!');
             });
     });
+
+    it('should contain an <input> element named principal', () => { 
+        pageObject
+            .evaluate(() => document.querySelector('input[name=principal]').name)
+            .then(headerText => {
+                expect(headerText).to.not.be.null;
+                expect(headerText).to.equal('notprincipal');
+            });
+    });
+
+    it('should correctly calculate mortgage', () =>
+        pageObject
+            .wait()
+            .type('input[name=principal]', 300000)
+            .type('input[name=interestRate]', 3.75)
+            .type('input[name=loanTerm]', 30)
+            .select('select[name=period]', 12)
+            .click('button#calculate')
+            .wait('#output')
+            .evaluate(() => document.querySelector('#output').innerText)
+            .then((outputText) => {
+                expect(outputText).to.equal('$1389.35');
+            })
+    ).timeout(6500);
 })
